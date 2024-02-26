8d575e93fe099a6589ebcd930e2e33889310ed5f
Rizel Thomas <r.thomas@gomeddo.com>
2024-02-26 16:12:51 +0100

    Added 'required' attribute to input fields
    Added some comments as well

--- src/pages/booking/Booking.js
+++ src/pages/booking/Booking.js
@@ -6,9 +6,10 @@ function Booking() {
 const location = useLocation();
 const { name, time, date} = location.state || {};
 
+// Add form submission handling logic
 const handleSubmit = (event) => {
     event.preventDefault();
-
+    // Form submission logic
     const path = '/dashboard';
     navigate(path);
 };
@@ -25,26 +26,26 @@ return (
             <h2>Confirm your {name} Booking</h2>
         </div>
         <div className = "booking-form-map">
-            {}
+            {/* Embed map component or image here */}
         </div>
         <div className = "booking-form-time">
             {date?.toLocaleString('default', { year: 'numeric', month: 'numeric', day: 'numeric'})} - {time}
         </div>
         <form onSubmit = {handleSubmit}>
             <div className = "booking-form-inputs">
-                <input type = "text" placeholder =  "First Name" />
-                <input type = "text" placeholder = "Last Name" />
-                <input type = "email" placeholder = "Email Address" />
-                <input type = "tel" placeholder = "Mobile Number" />
+                <input type = "text" placeholder =  "First Name" required />
+                <input type = "text" placeholder = "Last Name" required />
+                <input type = "email" placeholder = "Email Address" required />
+                <input type = "tel" placeholder = "Mobile Number" required />
             </div>
             <div className = "booking-form-consent">
-                <input type = "checkbox" id = "consentCheckbox" />
+                <input type = "checkbox" id = "consentCheckbox" required />
                 <label htmlFor = "consentCheckbox">
                     I give permission to save the data I have entered here and use this data to contact me.
                     More information in our privacy statement.
                 </label>
             </div>
-            
+
             <div className = "booking-form-actions">
                 <button type = "button" className = "booking-form-cancel" onClick = {cancel}>
                     Cancel
