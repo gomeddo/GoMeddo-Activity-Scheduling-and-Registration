05f5b267d7a2b4bc79d260b2d8879399fd9f9cb5
Rizel Thomas <r.thomas@gomeddo.com>
2024-02-29 15:34:57 +0100

    Tidied up layout component and remove the need for separate booking layout files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Map from "./pages/map/Map"
 import Dashboard from "./pages/dashboard/Dashboard";
-import BookingLayout from "./pages/BookingLayout";
 import Booking from "./pages/booking/Booking";
 
 function App() {
@@ -13,8 +12,6 @@ function App() {
           <Route element={<Navigate to="dashboard" replace />} index />
           <Route element={<Dashboard />} path="dashboard" />
           <Route element={<Map />} path="map" />
-        </Route>
-        <Route element={<BookingLayout />} path="/">
           <Route element={<Booking />} path="booking" />
         </Route>
       </Routes>
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Logo from '../assets/GoMeddoLogo.png';
 import IconMapFill from '../components/icons/IconMapFill';
 import IconCalendar from '../components/icons/IconCalendar';
@@ -7,6 +7,9 @@ import './Layout.css';
 
 function Layout() {
 
+    const location = useLocation();
+    const { pathname } = location;
+    const isBooking = pathname === '/booking';
     const handleToggle = () => {
         console.log('Toggle button clicked');
     }
@@ -14,17 +17,22 @@ function Layout() {
     return (
         <div className="layout-container">
             <div className="header-container">
-                <div className="header-placeholder"></div>
+                {/* Placeholder div to keep the title centered */}
+                {!isBooking &&
+                    <div className="header-placeholder"></div>
+                }
                 <div className="logo-container">
                     <img src={Logo} alt="GoMeddo Logo" />
                 </div>
-                <button className="toggle-button" onClick={handleToggle}>
-                    <span className="toggle-active"><IconCalendar /></span><span className="toggle-inactive"><IconMapFill /></span>
-                </button>
+                {!isBooking &&
+                    <button className="toggle-button" onClick={handleToggle}>
+                        <span className="toggle-active"><IconCalendar /></span><span className="toggle-inactive"><IconMapFill /></span>
+                    </button>
+                }
             </div>
 
             {/* Content container where nested routes will be rendered */}
-            <div className="content-container">
+            <div className={!isBooking ? 'content-container' : 'booking-content-container'}>
                 <Outlet />
             </div>
         </div>
