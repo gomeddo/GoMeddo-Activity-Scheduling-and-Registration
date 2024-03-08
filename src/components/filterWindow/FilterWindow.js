258113b34ff1d86f34e3f2872449a8bfd4120de9
Rizel Thomas <r.thomas@gomeddo.com>
2024-03-08 14:37:03 +0100

    Added dropdowns for instructors & locations to the body of filter window

--- src/components/filterWindow/FilterWindow.js
+++ src/components/filterWindow/FilterWindow.js
@@ -1,8 +1,13 @@
 import React, { useRef, useState } from "react";
 import "./FilterWindow.css";
 import IconFilter from "../icons/IconFilter";
+import resources from "../../i18n/resources";
+import { useTranslation } from "react-i18next";
+
 
 const FilterWindow = ({ onClose }) => {
+    const { t } = useTranslation();
+
     const [severityOptions, setSeverityOptions] = useState([
         { string: "Low" },
         { string: "Medium" },
@@ -17,6 +22,7 @@ const FilterWindow = ({ onClose }) => {
         { string: "Zumba" },
     ]);
 
+
     const handleApplyFilters = () => {
         onClose();
     };
@@ -25,9 +31,24 @@ const FilterWindow = ({ onClose }) => {
 
     return (
         <div className="filter-window-container">
-            <div className="filter-window-header"></div>
+            <div className="filter-window-header">{t(resources.button_filters)}</div>
             <div className="filter-window-body-container">
-                <div className="filter-window-body"></div>
+                <div className="filter-window-body">
+                    <form>
+                        <div className="filter-dropdown-container">
+                            <select className="filter-dropdown filter-dropdown1">
+                                <option value="Instuctors1">Instuctors1</option>
+                                <option value="Instuctors2">Instuctors2</option>
+                                <option value="Instuctors3">Instuctors3</option>
+                            </select>
+                            <select className="filter-dropdown filter-dropdown2">
+                                <option value="Location1">Location1</option>
+                                <option value="Location2">Location2</option>
+                                <option value="Location3">Location3</option>
+                            </select>
+                        </div>
+                    </form>
+                </div>
             </div>
             <div className="filter-window-footer"></div>
         </div>
