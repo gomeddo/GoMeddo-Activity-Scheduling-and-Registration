0d082d10ded604b2a8ff8cf5b819d7f3b625080f
Rizel Thomas <r.thomas@gomeddo.com>
2024-03-11 12:35:57 +0100

    Implemented Language Switcher
    - Add language switcher toggle
    - Added icons for each language

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -5,6 +5,10 @@ import Logo from "../assets/GoMeddoLogo.png";
 import IconMapFill from "../components/icons/IconMapFill";
 import IconCalendar from "../components/icons/IconCalendar";
 import "./Layout.css";
+import IconEnglish from "../components/icons/IconEnglish";
+import IconGerman from "../components/icons/IconGerman";
+import { useTranslation } from "react-i18next";
+import IconDutch from "../components/icons/IconDutch";
 
 function Layout() {
     const { isMapView, handleToggleMap } = useMapView();
@@ -13,11 +17,40 @@ function Layout() {
     const { pathname } = location;
     const isBooking = pathname === "/booking";
 
+    const { i18n } = useTranslation();
+
     return (
         <div className="layout-container">
             <div className="header-container">
                 {/* Placeholder div to keep the title centered */}
-                {!isBooking && <div className="header-placeholder" />}
+                {!isBooking && (
+                    <div className="toggle-container">
+                        <span
+                            className={
+                                i18n.language === "en" ? "toggle-active" : "toggle-inactive"
+                            }
+                            onClick={() => i18n.changeLanguage("en")}
+                        >
+                            <IconEnglish />
+                        </span>
+                        <span
+                            className={
+                                i18n.language === "de" ? "toggle-active" : "toggle-inactive"
+                            }
+                            onClick={() => i18n.changeLanguage("de")}
+                        >
+                            <IconGerman />
+                        </span>
+                        <span
+                            className={
+                                i18n.language === "nl" ? "toggle-active" : "toggle-inactive"
+                            }
+                            onClick={() => i18n.changeLanguage("nl")}
+                        >
+                            <IconDutch />
+                        </span>
+                    </div>
+                )}
                 <div className="logo-container">
                     {/* Image for the logo */}
                     <img src={Logo} alt="Logo" />
