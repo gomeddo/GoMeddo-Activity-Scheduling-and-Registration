642776bfa3e559a1cb966d23863dd2b4a6e76e2e
Rizel Thomas <r.thomas@gomeddo.com>
2024-03-11 13:36:03 +0100

    Created a goMeddo use hook

---
+++ src/hooks/useGoMeddo.js
@@ -0,0 +1,12 @@
+import { useMemo } from "react";
+import GoMeddo from "@gomeddo/sdk"
+
+export default function useGoMeddo() {
+    const apiKey = process.env.REACT_APP_API_KEY; // Retrieve the API key from environment variables
+
+    /* useMemo is used to memoize the GoMeddo instance. This ensures that the GoMeddo instance
+     is not recreated on every render unless the apiKey changes, which is specified as a dependency
+     in the dependency array [apiKey]. This optimization helps in avoiding unnecessary re-instantiations
+     and ensures that components using this hook do not unnecessarily re-render due to reference changes. */
+    return useMemo(() => new GoMeddo(apiKey), [apiKey]);
+}
