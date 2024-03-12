947bc9448891e98594133defd19006014330e5f0
Rizel Thomas <r.thomas@gomeddo.com>
2024-03-12 10:52:53 +0100

    Updated dashboard to use useGoMeddo hook to make api request & retrieve resource data, and handle loading and error states

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,12 +1,13 @@
-import { useState } from 'react';
-import { useMapView } from '../../providers/MapViewContext';
-import Activity from '../../components/activity/Activity';
-import Header from '../../components/header/Header';
-import WeeklyAgenda from '../../components/weeklyAgenda/WeeklyAgenda';
-import './Dashboard.css';
+import { useState, useEffect } from "react";
+import { useMapView } from "../../providers/MapViewContext";
+import Activity from "../../components/activity/Activity";
+import Header from "../../components/header/Header";
+import WeeklyAgenda from "../../components/weeklyAgenda/WeeklyAgenda";
+import useGoMeddo from "../../hooks/useGoMeddo";
+import "./Dashboard.css";
 
+// Custom hook to simulate fetching agenda items
 function useAgendaItems() {
-  // Hook to simulate fetching agenda items
   // Returns a structured list of agenda items divided into time sections
   return [
     {
@@ -50,44 +51,108 @@ function useAgendaItems() {
 
 function Dashboard() {
   const { isMapView } = useMapView();
-  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [selectedDate, handleSetSelectedDate] = useState(new Date());
   const [selectedClass, handleSetSelectedClass] = useState(undefined);
   const sections = useAgendaItems();
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const gm = useGoMeddo(); // Custom hook to use GoMeddo SDK functionality
+
+  useEffect(() => {
+    const formattedStartDate = selectedDate.toISOString().split("T")[0];
+    const endDate = new Date(selectedDate);
+    endDate.setDate(selectedDate.getDate() + 1);
+    const formattedEndDate = endDate.toISOString().split("T")[0];
+
+    const fetchData = async () => {
+      setLoading(true); // set loading state to true when fetching data
+      try {
+        //Fetch data logic using GoMeddo SDK and use this data to update the component state
+        let resources = await gm.buildResourceRequest();
+        //.includeAllResourcesAt(process.env.REACT_APP_RESOURCE_ID)
+        //.includeServices(true)
+        //.withAvailableSlotsBetween(formattedStartDate, formattedEndDate)
+        //.getResults()
+        //resources = [];
+        console.log(resources);
+        setData(resources[0]);
+        console.log(data);
+        setError(null);
+      } catch (error) {
+        // Error handling
+        setError(error);
+        setData(null);
+      } finally {
+        setTimeout(() => setLoading(false), 1000); // Simulate a delay in the loading state
+        // setLoading(true); // Ensure loading is set to false once the operation is complete
+      }
+    };
+
+    fetchData();
+  }, [selectedDate, gm, data]);
+
+  const handleDateChange = (newDate) => {
+    handleSetSelectedDate(newDate);
+  };
 
   return (
     <div className="dashboard-wrapper">
+      {/* Optional map view, displayed only if isMapView is true */}
       {isMapView && <div className="dashboard-map" />}
       <div className="dashboard-container">
         {/* Weekly agenda selector */}
         <WeeklyAgenda
           selectedDate={selectedDate}
-          handleDaySelected={(date) => setSelectedDate(date)}
+          onSelectDate={handleDateChange} // Update the agenda based on the selected date
         />
         {/* Container for each time section (Morning, Afternoon, Evening) */}
-        <div
-          className={
-            isMapView ? "sections-container-stacked" : "sections-container"
-          }
-        >
-          {sections.map((item, i) => (
-            <div key={i} className={isMapView ? "section-stacked" : "section"}>
-              {/* Section header (e.g., "Morning - 6:00am to 12:00pm") */}
-              <Header>{item.header}</Header>
-              {/* List of classes within the section */}
-              {item.items.map((item) => (
-                <Activity
-                  key={item.id} // Added key prop for item mapping
-                  {...item}
-                  active={selectedClass === item.id}
-                  selectedDate={selectedDate}
-                  onClick={(id) =>
-                    handleSetSelectedClass((state) => (state === id ? undefined : id))
-                  }
-                />
-              ))}
-            </div>
-          ))}
-        </div>
+        {loading && (
+          <div className="dashboard-loading">
+            Loading class schedule for{" "}
+            {selectedDate?.toLocaleString("default", {
+              weekday: "long",
+              day: "numeric",
+              month: "long",
+              year: "numeric",
+            })}
+            ...
+            <div className="dashboard-loading-spinner"></div>
+          </div>
+        )}
+        {error && <div>Error loading class schedule: {error.message}</div>}
+        {!loading && !error && (
+          <div
+            className={
+              isMapView ? "sections-container-stacked" : "sections-container"
+            }
+          >
+            {sections.map((item, i) => (
+              <div
+                key={i}
+                className={isMapView ? "section-stacked" : "section"}
+              >
+                {/* Section header (e.g., "Morning - 6:00am to 12:00pm") */}
+                <Header>{item.header}</Header>
+                {/* List of classes within the section */}
+                {item.items.map((item) => (
+                  <Activity
+                    key={item.id} // Added key prop for item mapping
+                    {...item}
+                    active={selectedClass === item.id}
+                    selectedDate={selectedDate}
+                    onClick={(id) =>
+                      handleSetSelectedClass((state) =>
+                        state === id ? undefined : id,
+                      )
+                    }
+                  />
+                ))}
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
