8b673c767afc5087aa3ff9bc360c4b68d36a3445
Rizel Thomas <r.thomas@gomeddo.com>
2024-03-28 13:50:48 +0100

    Adding loader to booking page after confirmation

--- src/pages/booking/Booking.js
+++ src/pages/booking/Booking.js
@@ -1,20 +1,22 @@
-import { useNavigate, useLocation } from 'react-router-dom';
-import { useState } from 'react';
-import './Booking.css';
+import React from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useState } from "react";
+import "./Booking.css";
 import { useTranslation } from "react-i18next";
 import resources from "../../i18n/resources";
-import { SObject } from '@gomeddo/sdk';
-import { buildReservationObj } from "./helpers.js"
-import useGoMeddo from '../../hooks/useGoMeddo.js';
+import { SObject } from "@gomeddo/sdk";
+import { buildReservationObj } from "./helpers.js";
+import useGoMeddo from "../../hooks/useGoMeddo.js";
 
 function Booking() {
     const location = useLocation(); // Accessing current location
     const { name, time, date } = location.state || {}; // Destructure name, time, and date from location state with fallback to empty object
     const reservation = buildReservationObj(location.state.reservation); // Build reservation object using data from location state
     const [hasConfirmed, setHasConfirmed] = useState(false); // State variable for tracking confirmation status
+    const [isLoading, setIsLoading] = useState(false); // State variable for tracking loading status
     const { t } = useTranslation();
     const gm = useGoMeddo();
-
+    const navigate = useNavigate(); // Hook for navigation
 
     // Add form submission handling logic
     const handleSubmit = async (event) => {
@@ -31,17 +33,17 @@ function Booking() {
         reservationContact.setCustomProperty("B25__Notes__c", contactData);
 
         try {
+            setIsLoading(true); // Set loading to true when data submission starts
             reservation.addReservationContact(reservationContact);
             await gm.updateReservation(reservation);
             console.log(reservation);
+            setHasConfirmed(true);
         } catch (error) {
             console.log(error);
+        } finally {
+            setIsLoading(false); // Set loading to false after data submission completes
         }
-
-        // Set confirmation status to true
-        setHasConfirmed(true);
     };
-    const navigate = useNavigate(); // Hook for navigation
 
     // Function to navigate to dashboard
     const handleCancel = () => {
@@ -56,86 +58,122 @@ function Booking() {
     };
 
     return (
-        <div className="booking-form-container"> {/* Container for booking form */}
-            {hasConfirmed ? ( // Conditional rendering based on confirmation status
-                <div className="booking-form-confirmation"> {/* Confirmation message */}
-                    <div className="booking-form-header">
-                        <h2>{t(resources.label_booking_confirmed)}</h2>
+        <div className="booking-form-container">
+            {" "}
+            {/* Container for booking form */}
+            <div className="booking-form">
+                {" "}
+                {/* Booking form */}
+                {isLoading ? ( // Show loader if isLoading is true
+                    <div className="booking-form-loading">
+                        {t(resources.message_booking_loading)}
+                        <div className="booking-form-loading-spinner"></div>{" "}
+                        {/* Loader spinner */}
                     </div>
-                    <div className="booking-form-confirmation-content">
-                        <p>{t(resources.message_booking_confirmed_thankyou)}</p>
-                        <p>{t(resources.message_booking_confirmed_confirmed)}</p>
-                        <p className="booking-form-confirmed-class">
-                            {t(resources.message_booking_confirmed_class_date, {
-                                name, date: date?.toLocaleString("default", {
-                                    weekday: "long",
-                                    day: "numeric",
-                                    month: "long",
-                                    year: "numeric",
-                                }), time
-                            })}
-                        </p>
-
-                        <p>{t(resources.message_booking_confirmed_email)}</p>
-                    </div>
-                    <div className="booking-form-actions">
-                        <button
-                            type="button"
-                            className="booking-form-button booking-form-back-button"
-                            onClick={handleBackToDashboard}
-                        >
-                            {t(resources.message_booking_confirmed_back)}
-                        </button>
-                    </div>
-                </div>
-            ) : (
-                <div className="booking-form">
-                    <div className="booking-form-header">
-                        <h2>{t(resources.label_booking_confirm_name, { name })}</h2>
-                    </div>
-                    <div className="booking-form-map">
-                        {/* Embed map component or image here */}
-                    </div>
-                    <div className="booking-form-time">
-                        {date?.toLocaleString("default", {
-                            year: "numeric",
-                            month: "numeric",
-                            day: "numeric",
-                        })}{" "}
-                        - {time}
-                    </div>
-                    <form onSubmit={handleSubmit}>
-                        <div className="booking-form-inputs">
-                            <input type="text" name="firstName" placeholder={t(resources.label_first_name)} required />
-                            <input type="text" name="lastName" placeholder={t(resources.label_last_name)} required />
-                            <input type="email" name="email" placeholder={t(resources.label_email_address)} required />
-                            <input type="tel" name="mobileNumber" placeholder={t(resources.label_mobile_number)} required />
+                ) : hasConfirmed ? ( // Show confirmation message if hasConfirmed is true
+                    <div className="booking-form-confirmation">
+                        {" "}
+                        {/* Confirmation message */}
+                        <div className="booking-form-header">
+                            <h2>{t(resources.label_booking_confirmed)}</h2>
                         </div>
-                        <div className="booking-form-consent">
-                            <input type="checkbox" id="consentCheckbox" required />
-                            <label htmlFor="consentCheckbox">
-                                {t(resources.message_booking_permission)}
-                            </label>
+                        <div className="booking-form-confirmation-content">
+                            <p>{t(resources.message_booking_confirmed_thankyou)}</p>
+                            <p>{t(resources.message_booking_confirmed_confirmed)}</p>
+                            <p className="booking-form-confirmed-class">
+                                {t(resources.message_booking_confirmed_class_date, {
+                                    name,
+                                    date: date?.toLocaleString("default", {
+                                        weekday: "long",
+                                        day: "numeric",
+                                        month: "long",
+                                        year: "numeric",
+                                    }),
+                                    time,
+                                })}
+                            </p>
+                            <p>{t(resources.message_booking_confirmed_email)}</p>
                         </div>
                         <div className="booking-form-actions">
                             <button
                                 type="button"
-                                className="booking-form-button booking-form-cancel"
-                                onClick={handleCancel}
+                                className="booking-form-button booking-form-back-button"
+                                onClick={handleBackToDashboard}
                             >
-                                {t(resources.button_cancel)}
-                            </button>
-                            <button
-                                type="submit"
-                                className="booking-form-button booking-form-submit"
-                            >
-                                {t(resources.button_confirm)}
+                                {t(resources.message_booking_confirmed_back)}
                             </button>
                         </div>
-                    </form>
-                </div>
-            )}
+                    </div>
+                ) : (
+                    <React.Fragment>
+                        <div className="booking-form-header">
+                            <h2>{t(resources.label_booking_confirm_name, { name })}</h2>
+                        </div>
+                        <div className="booking-form-map">
+                            {/* Embed map component or image here */}
+                        </div>
+                        <div className="booking-form-time">
+                            {date?.toLocaleString("default", {
+                                year: "numeric",
+                                month: "numeric",
+                                day: "numeric",
+                            })}{" "}
+                            - {time}
+                        </div>
+                        <form onSubmit={handleSubmit}>
+                            <div className="booking-form-inputs">
+                                <input
+                                    type="text"
+                                    name="firstName"
+                                    placeholder={t(resources.label_first_name)}
+                                    required
+                                />
+                                <input
+                                    type="text"
+                                    name="lastName"
+                                    placeholder={t(resources.label_last_name)}
+                                    required
+                                />
+                                <input
+                                    type="email"
+                                    name="email"
+                                    placeholder={t(resources.label_email_address)}
+                                    required
+                                />
+                                <input
+                                    type="tel"
+                                    name="mobileNumber"
+                                    placeholder={t(resources.label_mobile_number)}
+                                    required
+                                />
+                            </div>
+                            <div className="booking-form-consent">
+                                <input type="checkbox" id="consentCheckbox" required />
+                                <label htmlFor="consentCheckbox">
+                                    {t(resources.message_booking_permission)}
+                                </label>
+                            </div>
+                            <div className="booking-form-actions">
+                                <button
+                                    type="button"
+                                    className="booking-form-button booking-form-cancel"
+                                    onClick={handleCancel}
+                                >
+                                    {t(resources.button_cancel)}
+                                </button>
+                                <button
+                                    type="submit"
+                                    className="booking-form-button booking-form-submit"
+                                >
+                                    {t(resources.button_confirm)}
+                                </button>
+                            </div>
+                        </form>
+                    </React.Fragment>
+                )}
+            </div>
         </div>
     );
 }
+
 export default Booking;
