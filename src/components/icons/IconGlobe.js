5339fea79962fc4d6d706b3aa81407c5c7c339a9
Rizel Thomas <r.thomas@gomeddo.com>
2024-04-08 16:25:06 +0200

    Added new icons for language drop down

---
+++ src/components/icons/IconGlobe.js
@@ -0,0 +1,35 @@
+import * as React from "react";
+
+function IconGlobe(props) {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+    >
+      <g opacity="0.4">
+        <path
+          d="M8 15C11.866 15 15 11.866 15 8C15 4.13401 11.866 1 8 1C4.13401 1 1 4.13401 1 8C1 11.866 4.13401 15 8 15Z"
+          stroke="white"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M1 8H15"
+          stroke="white"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M7.99995 1C9.75085 2.91685 10.7459 5.40442 10.8 8C10.7459 10.5956 9.75085 13.0832 7.99995 15C6.24905 13.0832 5.25402 10.5956 5.19995 8C5.25402 5.40442 6.24905 2.91685 7.99995 1Z"
+          stroke="white"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+    </svg>
+  );
+}
+
+export default IconGlobe;
