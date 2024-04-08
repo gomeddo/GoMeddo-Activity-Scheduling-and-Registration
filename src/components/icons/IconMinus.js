cc5635d5c32ba9b1fa60897688f1610eb3f82a3c
Rizel Thomas <r.thomas@gomeddo.com>
2024-04-08 14:38:05 +0200

    Updating plus & minus icons

--- src/components/icons/IconMinus.js
+++ src/components/icons/IconMinus.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-function IconPlus(props) {
+function IconMinus(props) {
   return (
     <svg
       width="24"
@@ -11,18 +11,11 @@ function IconPlus(props) {
     >
       <path
         d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
-        fill="black"
-      />
-      <path
-        d="M12 8V16"
-        stroke="white"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        fill="white"
       />
       <path
         d="M8 12H16"
-        stroke="white"
+        stroke="black"
         stroke-width="2"
         stroke-linecap="round"
         stroke-linejoin="round"
@@ -31,4 +24,4 @@ function IconPlus(props) {
   );
 }
 
-export default IconPlus;
+export default IconMinus;
