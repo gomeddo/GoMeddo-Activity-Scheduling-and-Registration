af4acf21c4e7eb1818d98a6b106e93c356a8c8e7
Rizel Thomas <r.thomas@gomeddo.com>
2024-04-23 11:25:26 +0200

    Update useGoMeddo.js

--- src/hooks/useGoMeddo.js
+++ src/hooks/useGoMeddo.js
@@ -2,26 +2,26 @@ import { useState, useEffect, useMemo } from "react";
 import GoMeddo from "@gomeddo/sdk";
 
 export default function useGoMeddo() {
-    const [apiKey, setApiKey] = useState(""); // State variable to hold user-entered or localStorage API key
-    const [goMeddoInstance, setGoMeddoInstance] = useState(null); // State variable to hold GoMeddo instance
+  const [apiKey, setApiKey] = useState(""); // State variable to hold user-entered or localStorage API key
+  const [goMeddoInstance, setGoMeddoInstance] = useState(null); // State variable to hold GoMeddo instance
 
-    // Fetch API key from localStorage on component mount
-    useEffect(() => {
-        const fetchApiKeyFromLocalStorage = () => {
-            try {
-                const storedApiKey = localStorage.getItem("goMeddoApiKey");
-                if (storedApiKey) {
-                    setApiKey(storedApiKey);
-                    console.log("API key fetched from localStorage:", storedApiKey);
-                }
-            } catch (error) {
-                console.error("Error fetching API key from localStorage:", error);
-            }
-        };
+  // Fetch API key from localStorage on component mount
+  useEffect(() => {
+    const fetchApiKeyFromLocalStorage = () => {
+      try {
+        const storedApiKey = localStorage.getItem("goMeddoApiKey");
+        if (storedApiKey) {
+          setApiKey(storedApiKey);
+          console.log("API key fetched from localStorage:", storedApiKey);
+        }
+      } catch (error) {
+        console.error("Error fetching API key from localStorage:", error);
+      }
+    };
 
-        fetchApiKeyFromLocalStorage();
-    }, []); // Empty dependency array ensures it runs only on mount
+    fetchApiKeyFromLocalStorage();
+  }, []); // Empty dependency array ensures it runs only on mount
 
-    // Create GoMeddo instance only when apiKey is available and valid
-    return useMemo(() => new GoMeddo(apiKey), [apiKey]);
+  // Create GoMeddo instance only when apiKey is available and valid
+  return useMemo(() => new GoMeddo(apiKey), [apiKey]);
 }
